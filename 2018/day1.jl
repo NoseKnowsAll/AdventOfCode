d35a04d80903f7dbb572ef63b3a0643d0edfa8f6
NoseKnowsAll <masshellions@gmail.com>
2020-06-27 17:59:44 -0700

    Add SortedSet to improve complexity to O(nlogn)

--- 2018/day1.jl
+++ 2018/day1.jl
@@ -9,8 +9,10 @@ function compute_frequency_shift(filename="day1.input")
     return total_shift
 end
 
+using DataStructures
+
 function first_frequency(filename="day1.input")
-    all_shifts = Int64[]
+    all_shifts = SortedSet{Int64}()
     total_shift = 0
     iterations = 1
     MAX_ITERATIONS = 500
