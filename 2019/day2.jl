ba8a59d09a5f06e66fd3d2cc4323b0a83427d28d
NoseKnowsAll <masshellions@gmail.com>
2020-06-27 17:36:08 -0700

    Complete day2-2. Update naming convention

--- 2019/day2.jl
+++ 2019/day2.jl
@@ -8,23 +8,26 @@ end
 # Actually evaluates the opcode program, updating program as it runs
 function interpret_program!(program)
     index = 1
-    opcode = program[index]
-    while opcode != 99
+    instruction = program[index]
+    while instruction != 99
         from1 = program[index+1]+1 # ZERO-INDEXED INPUT
         from2 = program[index+2]+1 # ZERO-INDEXED INPUT
         to    = program[index+3]+1 # ZERO-INDEXED INPUT
 
-        if opcode == 1 # add
+        if instruction == 1 # add
             program[to] = program[from1] + program[from2]
-        elseif opcode == 2 # multiply
+        elseif instruction == 2 # multiply
             program[to] = program[from1] * program[from2]
         else
-            error("INVALID OPCODE")
+            # an error in interpreting program, not in running this simulation
+            #println("INVALID OPCODE INSTRUCTION")
+            return -1
         end
 
         index += 4
-        opcode = program[index]
+        instruction = program[index]
     end
+    return 0
 end
 
 # Solve day2-1
@@ -39,4 +42,28 @@ function restore_gravity(filename="day2.input")
     program[3] = 2
     interpret_program!(program)
     return program[1]
+end
+
+# Solve day2-2
+function determine_input(filename="day2.input")
+    file = open(filename)
+    string = readline(file)
+    close(file)
+
+    master_program = initialize_program(string)
+
+    # Test many different nouns and verbs until we output secret number
+    for noun = 0:99
+        for verb = 0:99
+            program = deepcopy(master_program)
+            program[2] = noun
+            program[3] = verb
+            error_code = interpret_program!(program)
+            if error_code == 0 # Sucessful completion
+                if program[1] == 19690720 # SECRET NUMBER
+                    return 100*noun+verb
+                end
+            end
+        end
+    end
 end
