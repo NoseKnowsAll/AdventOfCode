bbede36df07811db4ee04be26b47c1c98f5168e5
NoseKnowsAll <masshellions@gmail.com>
2020-06-29 23:20:51 -0700

    Refactor IntCode and day 2 in preparation of more general program

--- 2019/day2.jl
+++ 2019/day2.jl
@@ -1,34 +1,7 @@
-# Reinterprets the string from the file as an array of integers to run as a program
-function initialize_program(string)
-    array_string = split(string,',')
-    program = parse.(Int64,array_string)
-    return program
-end
+include("intcode.jl")
 
-# Actually evaluates the opcode program, updating program as it runs
-function interpret_program!(program)
-    index = 1
-    instruction = program[index]
-    while instruction != 99
-        from1 = program[index+1]+1 # ZERO-INDEXED INPUT
-        from2 = program[index+2]+1 # ZERO-INDEXED INPUT
-        to    = program[index+3]+1 # ZERO-INDEXED INPUT
-
-        if instruction == 1 # add
-            program[to] = program[from1] + program[from2]
-        elseif instruction == 2 # multiply
-            program[to] = program[from1] * program[from2]
-        else
-            # an error in interpreting program, not in running this simulation
-            #println("INVALID OPCODE INSTRUCTION")
-            return -1
-        end
-
-        index += 4
-        instruction = program[index]
-    end
-    return 0
-end
+global MAX_INSTRUCTION
+MAX_INSTRUCTION = 2
 
 # Solve day2-1
 function restore_gravity(filename="day2.input")
@@ -36,11 +9,11 @@ function restore_gravity(filename="day2.input")
     string = readline(file)
     close(file)
 
-    program = initialize_program(string)
+    program = IntCode.initialize_program(string)
     # 1202 program:
     program[2] = 12
     program[3] = 2
-    interpret_program!(program)
+    IntCode.interpret_program!(program)
     return program[1]
 end
 
@@ -50,7 +23,7 @@ function determine_input(filename="day2.input")
     string = readline(file)
     close(file)
 
-    master_program = initialize_program(string)
+    master_program = IntCode.initialize_program(string)
 
     # Test many different nouns and verbs until we output secret number
     for noun = 0:99
@@ -58,7 +31,7 @@ function determine_input(filename="day2.input")
             program = deepcopy(master_program)
             program[2] = noun
             program[3] = verb
-            error_code = interpret_program!(program)
+            error_code = IntCode.interpret_program!(program)
             if error_code == 0 # Sucessful completion
                 if program[1] == 19690720 # SECRET NUMBER
                     return 100*noun+verb
---
+++ 2019/intcode.jl
@@ -0,0 +1,192 @@
+module IntCode
+
+export OpCode
+export Instruction
+export initialize_program, interpret_program!
+
+# Enum just to specify which integer values correspond to which operations
+@enum OpCode begin
+    add=1
+    multiply=2
+    input=3
+    output=4
+    halt=99
+    unknown=-1
+end
+
+# For backward compatibility among the different days - don't introduce
+# functionality that wasn't yet introduced
+global MAX_INSTRUCTION
+MAX_INSTRUCTION = Int(halt)
+
+# Number of digits that compose the opcode
+OPCODE_LENGTH = 2
+
+# Successful completion of program
+SUCCESS = 0
+
+# Invalid OpCode
+INVALID_OPCODE = -1
+
+# Writing instruction cannot be in immediate mode
+IMMEDIATE_WRITE_ERROR = -2
+
+# Returns the instruction length of a given opcode
+function Base.length(opcode::OpCode)
+    global MAX_INSTRUCTION
+    if opcode == add
+        return 4
+    elseif opcode == multiply
+        return 4
+    elseif opcode == input && MAX_INSTRUCTION >= Int(input)
+        return 2
+    elseif opcode == output && MAX_INSTRUCTION >= Int(output)
+        return 2
+    elseif opcode == halt
+        return 1
+    else # Unrecognized opcode
+        return 1
+    end
+end
+
+# Mode of a given parameter
+@enum Modes begin
+    position=0  # interpret parameter as address
+    immediate=1 # interpret parameter as value
+end
+
+# Struct containing all the information about the current instruction
+struct Instruction
+    opcode::OpCode
+    length::Integer
+    modes #::Array{Modes}
+    parameters #::Array{Int32}
+
+    function Instruction(opcode_::OpCode, relevant_program)
+        # Handle unknown case immediately
+        global MAX_INSTRUCTION
+        if MAX_INSTRUCTION < Int(opcode_) && opcode_ != halt
+            opcode_ = unknown
+        end
+        length_ = length(opcode_)
+        instr_digits = digits(relevant_program[1], pad=length_)
+        modes_ = instr_digits[OPCODE_LENGTH+1:end]
+        parameters_ = relevant_program[2:length_]
+        new(opcode_, length_, modes_, parameters_)
+    end
+
+    function Instruction(opcode_::Integer, relevant_program)
+        # Handle unknown case immediately
+        global MAX_INSTRUCTION
+        if MAX_INSTRUCTION < opcode_ && opcode_ != halt
+            true_opcode = unknown
+        else
+            true_opcode = OpCode(opcode_)
+        end
+        length_ = length(true_opcode)
+        instr_digits = digits(relevant_program[1], pad=length_+1)
+        modes_ = instr_digits[OPCODE_LENGTH+1:end]
+        parameters_ = relevant_program[2:length_]
+        new(true_opcode, length_, modes_, parameters_)
+    end
+end
+
+function Base.show(io::IO, instruction::Instruction)
+    print(io, "Instruction{OpCode=$(instruction.opcode), ")
+    print(io, "len=$(instruction.length), ")
+    print(io, "modes=$(instruction.modes), ")
+    print(io, "params=$(instruction.parameters)}")
+end
+
+# Extracts information from instruction
+function interpret_instruction(program, index)
+    instr_digits = digits(program[index], pad=OPCODE_LENGTH)
+    opcode = sum([instr_digits[k]*10^(k-1) for k = 1:OPCODE_LENGTH])
+    relevant_program = program[index:index+length(OpCode(opcode))-1]
+    return Instruction(opcode, relevant_program)
+end
+
+# Modify program by evaluating instruction
+function eval_instruction!(program, instruction::Instruction)
+    global MAX_INSTRUCTION
+
+    if instruction.opcode == add
+        println(instruction)
+        val1 = 0
+        if instruction.modes[1] == Int(position)
+            val1 = program[instruction.parameters[1]+1] # ZERO-INDEXED INPUT
+        elseif instruction.modes[1] == Int(immediate)
+            val1 = instruction.parameters[1]
+        end
+        val2 = 0
+        if instruction.modes[2] == Int(position)
+            val2 = program[instruction.parameters[2]+1] # ZERO-INDEXED INPUT
+        elseif instruction.modes[2] == Int(immediate)
+            val2 = instruction.parameters[2]
+        end
+        val3 = instruction.parameters[3]+1 # ZERO-INDEXED INPUT
+        if instruction.modes[3] != Int(position)
+            error("adding to a value, not location")
+            return IMMEDIATE_WRITE_ERROR
+        end
+
+        # Actually add
+        program[val3] = val1+val2
+
+    elseif instruction.opcode == multiply
+        if instruction.modes[1] == Int(position)
+            val1 = program[instruction.parameters[1]+1] # ZERO-INDEXED INPUT
+        elseif instruction.modes[1] == Int(immediate)
+            val1 = instruction.parameters[1]
+        end
+        if instruction.modes[2] == Int(position)
+            val2 = program[instruction.parameters[2]+1] # ZERO-INDEXED INPUT
+        elseif instruction.modes[2] == Int(immediate)
+            val2 = instruction.parameters[2]
+        end
+        val3 = instruction.parameters[3]+1 # ZERO-INDEXED INPUT
+        if instruction.modes[3] != Int(position)
+            error("adding to a value, not location")
+            return IMMEDIATE_WRITE_ERROR
+        end
+
+        # Actually multiply
+        program[val3] = val1*val2
+
+    elseif instruction.opcode == input && MAX_INSTRUCTION >= Int(input) # input
+
+    elseif instruction.opcode == output && MAX_INSTRUCTION >= Int(output) # output
+
+    else
+        # an uknown opcode. Not necessarily an error
+        return INVALID_OPCODE
+    end
+    return SUCCESS
+end
+
+# Reinterprets the string from the file as an array of integers to run as a program
+function initialize_program(string)
+    array_string = split(string,',')
+    program = parse.(Int64,array_string)
+    return program
+end
+
+# Actually evaluates the opcode program, updating program as it runs
+function interpret_program!(program)
+    index = 1
+    instruction = interpret_instruction(program, index)
+
+    while instruction.opcode != halt
+        error_code = eval_instruction!(program, instruction)
+        if error_code != SUCCESS
+            return error_code
+        end
+
+        # Setup for next instruction
+        index += instruction.length
+        instruction = interpret_instruction(program,index)
+    end
+    return SUCCESS
+end
+
+end
