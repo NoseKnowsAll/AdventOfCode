feab1ac778c4f75aa9fa6d787b5f9c42a6e8ac1f
NoseKnowsAll <masshellions@gmail.com>
2020-07-04 13:50:32 -0700

    Separate computation by index. Complete day 12-2

--- 2019/day12.jl
+++ 2019/day12.jl
@@ -15,8 +15,8 @@ function read_init_positions(filename)::Moons
 end
 
 # The poor man's gravity
-function apply_force!(moons::Moons,i,j)
-    for d = 1:size(moons.positions,1)
+function apply_force!(moons::Moons,i,j, index)
+    for d in index
         vel_diff = sign(moons.positions[d,i] - moons.positions[d,j])
         moons.velocities[d,i] -= vel_diff
         moons.velocities[d,j] += vel_diff
@@ -24,23 +24,23 @@ function apply_force!(moons::Moons,i,j)
 end
 
 # Apply gravity amongst all the moons
-function apply_forces!(moons::Moons)
+function apply_forces!(moons::Moons, index)
     for i = 1:size(moons.positions,2)
         for j = i+1:size(moons.positions,2)
-            apply_force!(moons,i,j)
+            apply_force!(moons,i,j, index)
         end
     end
 end
 
 # Move positions acccording to velocity
-function apply_velocity!(moons::Moons)
-    moons.positions .+= moons.velocities
+function apply_velocity!(moons::Moons, index)
+    moons.positions[index,:] .+= moons.velocities[index,:]
 end
 
 # Time step the moons
-function timestep!(moons::Moons)
-    apply_forces!(moons)
-    apply_velocity!(moons)
+function timestep!(moons::Moons, index=1:size(moons.positions,1))
+    apply_forces!(moons,index)
+    apply_velocity!(moons,index)
 end
 
 # Poor man's energy of the system
@@ -57,4 +57,30 @@ function total_energy(filename="day12.input", timesteps=1000)
         timestep!(moons)
     end
     compute_energy(moons)
+end
+
+# Solves day 12-2
+function history_repeats_itself(filename="day12.input")
+    init_moons = read_init_positions(filename)
+    moons = deepcopy(init_moons)
+
+    # Simulate each dimension separately in order to get back to the init state
+    dims = size(moons.positions,1)
+    timesteps_to_repeat = zeros(Int,dims)
+    for d = 1:dims
+        timesteps = 0
+        while !(moons.positions[d,:] == init_moons.positions[d,:] &&
+            moons.velocities[d,:] == init_moons.velocities[d,:]) ||
+            timesteps == 0
+            timestep!(moons, d)
+            timesteps += 1
+        end
+        timesteps_to_repeat[d] = timesteps
+    end
+    # Compute the number of timesteps to repeat the entire state using lcm
+    total_lcm = 1
+    for d = 1:dims
+        total_lcm = lcm(timesteps_to_repeat[d],total_lcm)
+    end
+    return total_lcm
 end
