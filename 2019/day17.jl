b34626f0a34eab2c09157788631b6bc7deb8cca7
NoseKnowsAll <masshellions@gmail.com>
2020-07-08 16:02:56 -0700

    Complete day 17-2

--- 2019/day17.jl
+++ 2019/day17.jl
@@ -84,7 +84,7 @@ function init_program(filename)::IntCode.Program
 end
 
 # Create the Scaffolding struct
-function init_scaffolding(program::IntCode.Program)::Scaffolding
+function init_scaffolding!(program::IntCode.Program)::Scaffolding
     # Concatenate output characters to string
     scaffold_string = ""
     finished = false
@@ -94,6 +94,9 @@ function init_scaffolding(program::IntCode.Program)::Scaffolding
             finished = true
         else
             scaffold_string *= Char(program.outputs[end])
+            if length(scaffold_string)>1 && scaffold_string[end-1:end]=="\n\n"
+                break
+            end
         end
     end
     # end-2 to ignore trailing 2 \n characters
@@ -134,9 +137,71 @@ end
 # Solves day 17-1
 function alignment_parameters(filename="day17.input")
     program = init_program(filename)
-    scaffold = init_scaffolding(program)
-    println(scaffold)
+    scaffold = init_scaffolding!(program)
     # Returns the sum of the alignment parameters
     intersections = scaffold_intersections(scaffold)
     sum_alignment_parameters(intersections)
+end
+
+# Input string to program, one ASCII code at a time
+function input_argument!(program::IntCode.Program, input_string)
+    if length(input_string) > 21
+        error("routines are too long for input!")
+    end
+    inputs = Int.(collect(input_string))
+    for i = 1:length(inputs)
+        push!(program.inputs, inputs[i])
+        #IntCode.interpret_program!(program) # TODO: run here or no?
+    end
+end
+
+# Run program until output is \n
+function run_to_enter!(program::IntCode.Program, breakpoint='\n')
+    finished = false
+    while !finished
+        IntCode.interpret_program!(program)
+        if program.outputs[end] == Int(breakpoint)
+            finished = true
+        end
+    end
+end
+
+# Supply all arguments to program
+function supply_arguments!(program::IntCode.Program, solution,A,B,C,cont_feed)
+    input_argument!(program, solution)
+    run_to_enter!(program)
+    input_argument!(program, A)
+    run_to_enter!(program)
+    input_argument!(program, B)
+    run_to_enter!(program)
+    input_argument!(program, C)
+    run_to_enter!(program)
+    input_argument!(program, cont_feed)
+    run_to_enter!(program)
+    run_to_enter!(program)
+end
+
+# Solves day 17-2
+function space_dust(filename="day17.input")
+    program = init_program(filename)
+    # TODO: figure out these strings from the scaffolding itself
+    solution = "A,A,B,C,B,C,B,C,B,A\n"
+    A = "R,6,L,12,R,6\n"
+    B = "L,12,R,6,L,8,L,12\n"
+    C = "R,12,L,10,L,10\n"
+    continuous_feed = "n\n"
+
+    # Wake up robot and supply it its instructions
+    program.program[1] = 2
+    scaffold = init_scaffolding!(program)
+    #println(scaffold)
+    supply_arguments!(program, solution,A,B,C,continuous_feed)
+
+    # Run program and reach end of scaffolding
+    scaffold = init_scaffolding!(program)
+    #println(scaffold)
+
+    # Get final output
+    IntCode.interpret_program!(program)
+    return program.outputs[end]
 end
