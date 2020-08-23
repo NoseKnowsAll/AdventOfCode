c97eaf576abe9dfc53a3f0e0b90c19ad10b41787
NoseKnowsAll <masshellions@gmail.com>
2020-08-23 15:26:29 -0700

    Convert action_dict to sleep_time array

--- 2018/day4.jl
+++ 2018/day4.jl
@@ -9,18 +9,23 @@ function read_file(filename)
         action_dict[digit_val] = action
     end
 
-    all_action_dicts = Dict{Int64,SortedDict}()
+    all_sleep_times = Dict{Int64,Array{Tuple}}()
     curr_guard = 0
+    sleep_time = 0
+    awake_time = 0
     for (k,action) in action_dict
         if action[1] == 'G' # Guard # begins shift
             m = match(r"(\d+)",action)
             curr_guard = parse(Int64,m.captures[1])
-        else
-            so_far = get!(all_action_dicts, curr_guard, SortedDict{Int64,String}())
-            so_far[k] = action
+        elseif action[1] == 'f' # falls asleep
+            sleep_time = rem(k,100)
+        elseif action[1] == 'w' # wakes up
+            awake_time = rem(k,100)
+            so_far = get!(all_sleep_times, curr_guard, [])
+            push!(so_far, (sleep_time,awake_time))
         end
     end
-    return all_action_dicts
+    return all_sleep_times
 end
 
 # Interprets a given line of code into (1) time and (2) action string
@@ -34,23 +39,18 @@ function interpret_line(line::String)
 end
 
 # Compute the guard who sleeps the most
-function guard_most_asleep(all_action_dicts)
+function guard_most_asleep(all_sleep_times)
     asleep_minutes = Dict{Int64,Int64}()
     max_sleep_time = 0
     max_sleep_id = 0
-    for (guard_id, action_dict) in all_action_dicts
+    for (guard_id, sleep_times) in all_sleep_times
         asleep_minutes[guard_id] = 0
-        start_sleep_minute = 0
-        for (k,action) in action_dict
-            if action[1] == 'f' # falls asleep
-                start_sleep_minute = rem(k,100)
-            elseif action[1] == 'w' # wakes up
-                total_sleep_minutes = rem(k,100) - start_sleep_minute
-                asleep_minutes[guard_id] += total_sleep_minutes
-                if asleep_minutes[guard_id] > max_sleep_time
-                    max_sleep_time = asleep_minutes[guard_id]
-                    max_sleep_id = guard_id
-                end
+        for (sleep_minute, awake_minute) in sleep_times
+            total_sleep_minutes = awake_minute - sleep_minute
+            asleep_minutes[guard_id] += total_sleep_minutes
+            if asleep_minutes[guard_id] > max_sleep_time
+                max_sleep_time = asleep_minutes[guard_id]
+                max_sleep_id = guard_id
             end
         end
     end
@@ -58,44 +58,34 @@ function guard_most_asleep(all_action_dicts)
 end
 
 # Return the specific minute this guard is most often asleep from their action_dict
-function find_most_asleep_minute(action_dict)
+function find_most_asleep_minute(sleep_times)
     MINUTES_PER_HOUR = 60
     minutes = zeros(MINUTES_PER_HOUR)
-    start_sleep_minute = 0
-    for (k,action) in action_dict
-        if action[1] == 'f' # falls asleep
-            start_sleep_minute = rem(k,100)
-        elseif action[1] == 'w' # wakes up
-            minutes[start_sleep_minute+1:rem(k,100)] .+= 1 # ONE-INDEXED
-        end
+    for (sleep_minute,awake_minute) in sleep_times
+        minutes[sleep_minute+1:awake_minute] .+= 1 # ONE-INDEXED
     end
     (maxval, minute) = findmax(minutes)
     return minute-1 # ZERO-INDEXED
 end
 
-# Solves day 4-1
+# Solves Day 4-1
 function most_asleep(filename="day4.input")
-    all_action_dicts = read_file(filename)
-    max_sleep_guard_id = guard_most_asleep(all_action_dicts)
-    minute = find_most_asleep_minute(all_action_dicts[max_sleep_guard_id])
+    all_sleep_times = read_file(filename)
+    max_sleep_guard_id = guard_most_asleep(all_sleep_times)
+    minute = find_most_asleep_minute(all_sleep_times[max_sleep_guard_id])
     return max_sleep_guard_id*minute
 end
 
 # Find the guard and minute on which this guard is most frequently asleep
-function guard_most_asleep_minute(all_action_dicts)
+function guard_most_asleep_minute(all_sleep_times)
     max_asleep_times = 0
     asleep_minute = 0
     asleep_guard = 0
-    for (guard,action_dict) in all_action_dicts
+    for (guard,sleep_times) in all_sleep_times
         MINUTES_PER_HOUR = 60
         minutes = zeros(MINUTES_PER_HOUR)
-        start_sleep_minute = 0
-        for (k,action) in action_dict
-            if action[1] == 'f' # falls asleep
-                start_sleep_minute = rem(k,100)
-            elseif action[1] == 'w' # wakes up
-                minutes[start_sleep_minute+1:rem(k,100)] .+= 1 # ONE-INDEXED
-            end
+        for (sleep_minute, awake_minute) in sleep_times
+            minutes[sleep_minute+1:awake_minute] .+= 1 # ONE-INDEXED
         end
         (maxval, minute) = findmax(minutes)
         if maxval > max_asleep_times
@@ -107,9 +97,9 @@ function guard_most_asleep_minute(all_action_dicts)
     return (asleep_guard, asleep_minute)
 end
 
-# Solves day 4-2
+# Solves Day 4-2
 function same_minute_asleep(filename="day4.input")
-    all_action_dicts = read_file(filename)
-    (guard_id,minute) = guard_most_asleep_minute(all_action_dicts)
+    all_sleep_times = read_file(filename)
+    (guard_id,minute) = guard_most_asleep_minute(all_sleep_times)
     return guard_id*minute
 end
